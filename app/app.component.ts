6a04cdb92bcd41db397c03d4eee6390afc4d6d58
David Nichols <dnich@Davids-MacBook-Pro-2.local>
2016-03-28 22:10:21 -0400

    Boostrap Angular2 app

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+/// <reference path="./../node_modules/angular2/typings/browser.d.ts" />
+
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'acro',
+  template: '<p>Hello, world!</p>'
+})
+
+// Application Root Component:
+
+export class AppComponent { }
