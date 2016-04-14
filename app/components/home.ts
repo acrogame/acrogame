248fa0d1d023dfca04091771f93423c2c4d4b02d
David Nichols <davenich@gmail.com>
2016-04-13 21:30:44 -0400

    Wire-up rooms to home

--- app/components/home.ts
+++ app/components/home.ts
@@ -1,12 +1,20 @@
 import {Component}                  from 'angular2/core';
 import {ROUTER_DIRECTIVES, Router}  from 'angular2/router';
+import {FirebaseEventPipe}          from './../pipes/pipes.firebaseevent';
+import {Config}                     from './../config/config.acro';
 
 @Component({
   selector: 'home',
   templateUrl: 'app/templates/templates.home.html',
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  pipes: [FirebaseEventPipe]
 })
 
 export class Home { 
-  // todo
+  roomsUrl: string;
+  
+  constructor(private config: Config) {
+    var fbUrl = config.get('firebaseUrl');
+    this.roomsUrl = `${fbUrl}/rooms`;
+  }
 }
